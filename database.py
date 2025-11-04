3b967b4b38d683bc13ba00b44c952ee9fd808baf
shanisaalmawadah <shanisaalmawadah@gmail.com>
2025-11-04 16:00:39 +0700

    Menghapus file dan folder yang tidak diperlukan

--- database.py
+++
@@ -1,9 +0,0 @@
-echo "import mysql.connector
-def koneksi():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='nama_database'
-    )
-print('Koneksi ke database berhasil!')" > database.py
